b680478bcd914703823d63aa9438d7b0a21e7247
Mike <mike42@hotmail.de>
2019-04-03 19:03:36 +0200

    Frist strike

---
+++ lib/brainfuck.ex
@@ -0,0 +1,18 @@
+defmodule Brainfuck do
+  @moduledoc """
+  Documentation for Brainfuck.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Brainfuck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
