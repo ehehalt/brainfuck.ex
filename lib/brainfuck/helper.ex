bb453cc0d911d6911f0a8ea53f8a2482c7945cb3
Mike <mike42@hotmail.de>
2019-04-04 19:21:14 +0200

    Brainfuck.Helper documented and doc tested.

--- lib/brainfuck/helper.ex
+++ lib/brainfuck/helper.ex
@@ -1,15 +1,70 @@
 defmodule Brainfuck.Helper do
+  @moduledoc """
+  Helper functions for increase and decrease elements in a list:
 
-  def inc_at(list, addr) when addr >= 0 do
+  - inc_at/2
+  - dec_at/2
+
+  Helper function for increase and decrease single elements:
+
+  - inc/1
+  - dec/1
+
+  All functions calculate with modulo 256 overflow.
+  """
+
+  @doc """
+  Increase an element of a list at the specific addr with modulo 256 overflow calculation.
+
+  ## Examples
+
+      iex> Brainfuck.Helper.inc_at([0, 0], 1)
+      [0, 1]
+
+      iex> Brainfuck.Helper.inc_at([0, 255], 1)
+      [0, 0]
+
+  """
+  def inc_at(list, addr) when is_list(list) and is_integer(addr) and addr >= 0 and addr < length(list) do
     List.update_at(list, addr, &(Brainfuck.Helper.inc/1))
   end
 
-  def dec_at(list, addr) when addr >= 0 do
+  @doc """
+  Decrease an element of a list at the specific addr with modulo 256 overflow calculation.
+
+  ## Examples
+
+      iex> Brainfuck.Helper.dec_at([0, 0], 1)
+      [0, 255]
+
+      iex> Brainfuck.Helper.dec_at([0, 2], 1)
+      [0, 1]
+
+  """
+  def dec_at(list, addr) when is_list(list) and is_integer(addr) and addr >= 0 and addr < length(list) do
     List.update_at(list, addr, &(Brainfuck.Helper.dec/1))
   end
 
+  @doc """
+  Increase with modulo 256 overflow calculation.
+
+  ## Examples
+
+      iex> Brainfuck.Helper.inc(0)
+      1
+
+  """
   def inc(value), do: rem(value + 1, 256)
 
+  @doc """
+  Decrease with modulo 256 overflow calculation.
+
+  ## Examples
+
+      iex> Brainfuck.Helper.dec(0)
+      255
+
+  """
   def dec(value), do: rem(256 + (value - 1), 256)
 
 end
